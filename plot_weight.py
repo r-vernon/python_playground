d16c78a0fe51bfaa8990c803d2a78f966095ce69
Richard Vernon <richard.vernon88@gmail.com>
2023-01-24 16:30:41 +0000

    removed traz

--- plot_weight.py
+++ plot_weight.py
@@ -11,6 +11,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+# set dpi for figures
+plt.rcParams["figure.dpi"] = 300
+
 #%% import file
 
 # import the file
@@ -39,13 +42,13 @@ weight_dat.drop_duplicates(inplace=True)
 #%% set some key dates to flag
 
 # time on elvanse
-elvDate = (datetime(2017,12,5),datetime(2020,2,6))
+elvDate = (datetime(2017,12,5),datetime(2020,1,31))
 
 # time on sertraline
 sertDate = (datetime(2020,2,14),datetime(2021,2,20))
 
-# time on trazadone
-trazDate = (datetime(2021,8,18),datetime(2021,9,15))
+# # time on trazadone not including as so minimal
+# trazDate = (datetime(2021,8,18),datetime(2021,9,15))
 
 # time on fluoxetine
 fluoxDate = (datetime(2021,9,20),datetime(2022,3,18))
@@ -74,11 +77,11 @@ plt.ylabel('Weight (Kgs)')
 plt.ylim(70,150)
 
 # fill areas of interest
-ax.fill_betweenx((70,150), elvDate[0],   elvDate[1],   alpha=0.2)
-ax.fill_betweenx((70,150), sertDate[0],  sertDate[1],  alpha=0.2)
-ax.fill_betweenx((70,150), trazDate[0],  trazDate[1],  alpha=0.2)
-ax.fill_betweenx((70,150), fluoxDate[0], fluoxDate[1], alpha=0.2)
-ax.fill_betweenx((70,150), concDate[0],  concDate[1],  alpha=0.2)
+ax.fill_betweenx((70,150), elvDate[0],   elvDate[1],   alpha=0.2, color='steelblue')
+ax.fill_betweenx((70,150), sertDate[0],  sertDate[1],  alpha=0.2, color='tomato')
+# ax.fill_betweenx((70,150), trazDate[0],  trazDate[1],  alpha=0.2, color='orange')
+ax.fill_betweenx((70,150), fluoxDate[0], fluoxDate[1], alpha=0.2, color='gold')
+ax.fill_betweenx((70,150), concDate[0],  concDate[1],  alpha=0.2, color='cadetblue')
 
 # show the glory
 plt.show()
