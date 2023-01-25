4c8f09e76622cae5f5f82238505d5a1553dd823f
Richard Vernon <richard.vernon88@gmail.com>
2023-01-25 15:03:05 +0000

    more folder shuffling

---
+++ mahalanobis/mahalanobis_test.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 14:14:28 2022
+
+@author: richard
+"""
+
+# import shiz
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% first lets create some correlated data
+# corr(x,y) = cov(x,y)/(sqrt(var(x)*var(y)))
+
+# set random seed for consistency
+np.random.seed(42)
+
+# set target correlation
+targCorr = 0.6 
+
+# set properties of data
+nSamples = 1000 # Number of samples
+xy_M = (6,6)    # Mean (centre) of data
+xy_SD = (2,1)   # SD (spread) of data
+
+# calculate required covariance
+xy_cov = targCorr*(xy_SD[0]*xy_SD[1])
+
+# create target covariance matrix (probably better ways to do this!)
+covMat = np.full((2,2),xy_cov)
+np.fill_diagonal(covMat,np.square(xy_SD))
+
+# perform cholesky decomposition
+# decomposes Cov into lower triangle matrix L, where L.L' = Cov
+L = np.linalg.cholesky(covMat)
+
+# create uncorrelated normal data
+# multiply by decomposition
+# shift centre
+xy = np.random.standard_normal((2,nSamples))
+xy = np.dot(L,xy)
+xy = xy + np.array(xy_M).reshape(2,1)
+
+# correlate to check
+xy_r = np.corrcoef(xy[0,:],xy[1,:])[0,1]
+
+# calculate actual centre (not target)
+xy_cent = np.mean(xy,1)
+
+#%% plot that shit
+
+fig, ax = plt.subplots()
+
+ax.scatter(xy[0,:],xy[1,:],s=48,c='black',marker='.',alpha=0.6)
+ax.scatter(xy_cent[0],xy_cent[1],s=40,c='red',marker='x')
+
+ax.set(title='Correlated Data', \
+       xlim=(-2,14), xticks=np.arange(-2,15,2), xlabel='X Data', \
+       ylim=(-2,14),  ylabel='Y Data')
+ax.set_aspect('equal', adjustable='box')
+
+plt.show()
+
+#%% create two potential data points
+# one along trendline at reasonable (2SD) distance
+# one at same dist., rotated 90deg CCW - outlier!
+
+# line of best fit for data
+m1,c1 = np.polyfit(xy[0,:], xy[1,:], 1)
+
+# calculate lines orthogonal to that one
+m2 = -1/m1                # when orth. m1*m2=-1
+c2 = xy[1,:] - m2*xy[0,:] # y = mx+c, so c = y-mx
+
+# calculate intercept points for each of those lines (basic algebra)
+icpt_xy = np.empty_like(xy)
+icpt_xy[0,:] = (c1-c2) / (m2-m1)    # x
+icpt_xy[1,:] = m1*icpt_xy[0,:] + c1 # y
+
+# calculate distance of each intercept point from centre, along trendline
+# transpose for subtraction as trailing axes apparently need same dim. 
+# - xy_cent is (2,) so icpt_xy must be (*,2)
+icpt_dist = (icpt_xy.T - xy_cent).T # x, y deltas from centre
+icpt_dist = np.sqrt(np.sum(np.square(icpt_dist),axis=0)) # calc. hypot
+
+# preallocate potential new points
+pts = np.empty((2,2))
+
+# calculate point two stdevs along trendline from centre
+targDist = 2*np.std(icpt_dist) # target distance
+m1_ang = np.arctan(m1)         # angle between trendline and x-axis
+pts[0,:] = xy_cent + targDist*np.array((np.cos(m1_ang), np.sin(m1_ang)))
+
+# create rotated point
+th = np.deg2rad(-90)
+c, s = np.cos(th), np.sin(th)
+rotMat = np.array(((c, -s), (s, c)))
+pts[1,:] = xy_cent + ((pts[0,:]-xy_cent) @ rotMat)
+
+#%% plot those markers
+
+fig, ax = plt.subplots()
+
+ax.scatter(xy[0,:],xy[1,:],s=48,c='black',marker='.',alpha=0.6)
+#ax.plot(xy[0,:],m1*xy[0,:]+c1)
+ax.scatter(xy_cent[0],xy_cent[1],s=40,c='red',marker='x')
+ax.scatter(pts[0,0],pts[0,1],s=40,c='blue',marker='o')
+ax.scatter(pts[1,0],pts[1,1],s=40,c='green',marker='o')
+
+ax.set(title='Correlated Data', \
+       xlim=(-2,14), xticks=np.arange(-2,15,2), xlabel='X Data', \
+       ylim=(-2,14),  ylabel='Y Data')
+ax.set_aspect('equal', adjustable='box')
+
+plt.show()
+
+#%% create a euclidian distance plot
+
+# create meshgrid based on axis limits above
+xlin = np.linspace(-2,14,100)
+ylin = np.linspace(-2,14,100)
+mgridX,mgridY = np.meshgrid(xlin,ylin)
+
+# calculate euclidian distance
+eucDist = np.sqrt(np.square(mgridX-xy_cent[0]) + np.square(mgridY-xy_cent[1]))
+
+#%% plot that shit again
+
+fig, ax = plt.subplots()
+
+ax.scatter(xy[0,:],xy[1,:],s=48,c='black',marker='.',alpha=0.6)
+ax.contourf(mgridX,mgridY,eucDist,1000,cmap=plt.cm.plasma_r,alpha=0.8,antialiased='true')
+ax.scatter(xy_cent[0],xy_cent[1],s=40,c='red',marker='x')
+
+ax.set(title='Euclidian Distance Map', \
+       xlim=(-2,14), xticks=np.arange(-2,15,2), xlabel='X Data', \
+       ylim=(-2,14),  ylabel='Y Data')
+ax.set_aspect('equal', adjustable='box')
+
+plt.show()
+
+#%% create a mahalanobis distance plot
+
+# calculate inverse of the covariance matrix
+inv_covMat = np.linalg.inv(np.cov(xy))
+
+# vectorise and demean earlier meshgrid
+mgridXY = np.vstack((np.matrix.flatten(mgridX-xy_cent[0]),\
+                     np.matrix.flatten(mgridY-xy_cent[1])))
+
+mahaDist = np.diagonal(mgridXY.T @ inv_covMat @ mgridXY)
+mahaDist = np.reshape(np.sqrt(mahaDist),(100,100))
+
+#%% plot that shit once more
+
+fig, ax = plt.subplots()
+
+ax.scatter(xy[0,:],xy[1,:],s=48,c='black',marker='.',alpha=0.6)
+ax.contourf(mgridX,mgridY,mahaDist,1000,cmap=plt.cm.plasma_r,alpha=0.8,antialiased='true')
+ax.scatter(xy_cent[0],xy_cent[1],s=40,c='red',marker='x')
+
+ax.set(title='Mahalanobis Distance Map', \
+       xlim=(-2,14), xticks=np.arange(-2,15,2), xlabel='X Data', \
+       ylim=(-2,14),  ylabel='Y Data')
+ax.set_aspect('equal', adjustable='box')
+
+plt.show()
---
+++ weightGraph/plot_weight.py
@@ -0,0 +1,150 @@
+ #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 16:18:13 2023
+
+@author: richard
+"""
+
+# import necessary stuff
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# set dpi for figures
+plt.rcParams["figure.dpi"] = 300
+
+#%% import file
+
+# import the file
+weight_dat = pd.read_csv('/home/richard/Documents/Python/weightGraph/weight.csv', \
+                         usecols=['Date','Weight (kg)'])
+    
+# rename columns
+weight_dat.rename(columns={'Weight (kg)':'Weight'},inplace=True)
+
+# make sure dates are set as dates!
+weight_dat["Date"] = pd.to_datetime(weight_dat["Date"])
+
+# reverse ordering so chronological
+weight_dat.sort_values(by=["Date"],inplace=True)
+
+# delete rows before 25/05/16 as likely erroneous
+weight_dat = weight_dat[~(weight_dat['Date'] < '2016-05-25')]
+
+# normalise the dates (remove times) as we don't care what time we weighed ourself
+# helps duplicate check later
+weight_dat['Date'] = weight_dat['Date'].dt.normalize()
+
+# remove duplicates (same day, same weight) - below does equivelant
+# weight_dat.drop_duplicates(inplace=True)
+
+# for any duplicates (same day, multiple weights) average
+# also sets date as index
+weight_dat = weight_dat.groupby('Date').mean()
+
+#%% smooth the weight data
+
+# upsample so every day is represented
+weight_dat = weight_dat.asfreq(freq='D')
+
+# # just demonstrate vast majority of cases it's one missing day at most
+# # interpolation should be fine!
+# nullIdx = np.arange(0,len(weight_dat))
+# nullIdx = np.diff(nullIdx[weight_dat['Weight'].isnull()])
+# plt.hist(nullIdx,bins=np.arange(1,10))
+
+# interpolate missing days (sci-pi cubic spline interpolation)
+# weight_dat['iWeight'] = weight_dat.interpolate(method='spline', order=3)
+weight_dat['iWeight'] = weight_dat.interpolate(method='time')
+
+# set window size and calculate SD (based on FWHM) 
+winSz = 30
+winSD = winSz/(2*np.sqrt(2*np.log(2)))
+
+# smooth the data
+weight_dat['sWeight'] = weight_dat['iWeight'].rolling(window=winSz, min_periods=1, \
+                                     win_type='gaussian',center=True).mean(std=winSD)
+
+
+#%% set some key dates to flag
+
+# time on elvanse
+elvDate = (datetime(2017,12,5),datetime(2020,1,31))
+
+# time on sertraline
+sertDate = (datetime(2020,2,14),datetime(2021,2,20))
+
+# # time on trazadone not including as so minimal
+# trazDate = (datetime(2021,8,18),datetime(2021,9,15))
+
+# time on fluoxetine
+fluoxDate = (datetime(2021,9,20),datetime(2022,3,18))
+
+# time on concerta
+concDate = (datetime(2022,9,28),weight_dat.index.max())
+
+# sleep apnea diagnosed
+apneaDate = (datetime(2020,10,19))
+
+#%% plot the data
+
+fig, ax = plt.subplots()
+
+# plot the dots using actual data 
+plt.plot(weight_dat.index,weight_dat['Weight'],'o',markersize=1, color='black')
+
+# plot the line on top
+plt.plot(weight_dat.index,weight_dat['sWeight'],'-', alpha=0.8)
+
+# set axis lables
+plt.xlabel('Date')
+plt.ylabel('Weight (Kgs)')
+
+# set y axis limits
+plt.ylim(70,150)
+
+# fill areas of interest
+ax.fill_betweenx((70,150), elvDate[0],   elvDate[1],   alpha=0.2, color='steelblue')
+ax.fill_betweenx((70,150), sertDate[0],  sertDate[1],  alpha=0.2, color='tomato')
+# ax.fill_betweenx((70,150), trazDate[0],  trazDate[1],  alpha=0.2, color='orange')
+ax.fill_betweenx((70,150), fluoxDate[0], fluoxDate[1], alpha=0.2, color='gold')
+ax.fill_betweenx((70,150), concDate[0],  concDate[1],  alpha=0.2, color='cadetblue')
+
+# # add a dashed line for sleep apnea treatment
+# plt.plot((apneaDate,apneaDate),(70,150),'k--')
+
+# add text labels
+ax.text(elvDate[0]+((elvDate[1]-elvDate[0])/2),72,'Elvanse',ha='center',size='small')
+ax.text(sertDate[0]+((sertDate[1]-sertDate[0])/2),72,'Zoloft',ha='center',size='small')
+ax.text(fluoxDate[0]+((fluoxDate[1]-fluoxDate[0])/2),72,'Prozac',ha='center',size='small')
+ax.text(concDate[0]+((concDate[1]-concDate[0])/2),72,'Concerta',ha='center',size='small')
+
+# save the figure
+plt.savefig('/home/richard/Documents/Python/weightGraph/weightGraph.png',dpi=150, pad_inches=0)
+
+# show the glory
+plt.show()
+
+#%% plot a subset
+
+elvDat = weight_dat[elvDate[0]:elvDate[1]]
+
+fig, ax = plt.subplots()
+
+# plot the dots using actual data 
+plt.plot(elvDat.index,elvDat['Weight'],'o',markersize=1, color='black')
+
+# plot the line on top
+plt.plot(elvDat.index,elvDat['sWeight'],'-', alpha=0.8)
+
+# set axis lables
+plt.xlabel('Date')
+plt.ylabel('Weight (Kgs)')
+
+# set y axis limits
+plt.ylim(80,110)
+
+# show the glory
+plt.show()
