efda1a62faf9e5c14c563c15428cd1a40bbc199b
Richard Vernon <40852586+r-vernon@users.noreply.github.com>
2023-01-30 16:16:36 +0000

    read in first year data

--- googleTrends/gTrends_depression.py
+++ googleTrends/gTrends_depression.py
@@ -6,3 +6,19 @@ Created on Mon Jan 30 15:11:15 2023
 @author: richard
 """
 
+
+# import necessary stuff
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import signal
+
+# set directory
+os.chdir('/home/richard/Documents/Python/googleTrends/')
+
+#%% read in the data
+
+# skip 2 rows, treat next (row 0) as header, rename cols, parse 1st col as date
+df = pd.read_csv('./data/Depression_2006.csv',skiprows=2,\
+                 header=0,names=['Date','Freq'],parse_dates=[0])
+
